8cac82044530c61516f862bf959eb3f72bef8e02
jae <wod6983@naver.com>
2025-08-25 22:02:46 +0900

    Update SafeAreaFitter.cs

--- Assets/Scripts/UI/SafeAreaFitter.cs
+++ Assets/Scripts/UI/SafeAreaFitter.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
 
+[RequireComponent(typeof(RectTransform))]
 public class SafeAreaFitter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    RectTransform rt;
+    Rect lastSafe;
+    Vector2Int lastRes;
+    ScreenOrientation lastOri;
 
-    // Update is called once per frame
+    void Awake() => rt = GetComponent<RectTransform>();
+    void OnEnable() => Apply();
     void Update()
     {
-        
+        if (Screen.safeArea != lastSafe ||
+            new Vector2Int(Screen.width, Screen.height) != lastRes ||
+            Screen.orientation != lastOri)
+            Apply();
+    }
+    void Apply()
+    {
+        var pixelSafe = Screen.safeArea;
+        lastSafe = pixelSafe;
+        lastRes  = new Vector2Int(Screen.width, Screen.height);
+        lastOri  = Screen.orientation;
+
+        // Canvas 기준으로 정규화
+        var anchorMin = pixelSafe.position;
+        var anchorMax = pixelSafe.position + pixelSafe.size;
+        anchorMin.x /= Screen.width;  anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;  anchorMax.y /= Screen.height;
+        rt.anchorMin = anchorMin;
+        rt.anchorMax = anchorMax;
+        rt.offsetMin = Vector2.zero;
+        rt.offsetMax = Vector2.zero;
     }
 }
