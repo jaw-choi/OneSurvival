d405a7d0241bc6266202b2960c9f286aa82c8375
jae <wod6983@naver.com>
2025-09-02 19:29:22 +0900

    Fix Reposition bug
    Fix Reposition bug
    ontrigger대신 lateupdate안에 가드 넣고 최소한으로 업데이트하고 타일 하나씩 옮기는게 아니라 전체 타일을 옮겨서 대각선으로 이동할때 가끔 나오던 버그나지않게함

---
+++ Assets/Scripts/UI/GridSnapManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class GridSnapManager : MonoBehaviour
+{
+    // 2×2 예시: [BL, BR, TL, TR]
+    [SerializeField] Transform player;
+    [SerializeField] Transform[] chunks;
+    [SerializeField] float stepX = 24f, stepY = 24f;
+    [SerializeField] Vector2 anchor = Vector2.zero;
+
+    Vector2Int lastIdx = new Vector2Int(int.MinValue, int.MinValue);
+
+    void LateUpdate()
+    {
+        var p = player.position;
+        int ix = Mathf.FloorToInt((p.x - anchor.x) / stepX);
+        int iy = Mathf.FloorToInt((p.y - anchor.y) / stepY);
+
+        if (ix == lastIdx.x && iy == lastIdx.y) return; // 대부분 프레임은 여기서 끝
+        lastIdx.x = ix; lastIdx.y = iy;
+
+        float blx = anchor.x + ix * stepX;
+        float bly = anchor.y + iy * stepY;
+
+        if (chunks[0]) chunks[0].position = new Vector3(blx, bly, chunks[0].position.z); // BL
+        if (chunks[1]) chunks[1].position = new Vector3(blx + stepX, bly, chunks[1].position.z); // BR
+        if (chunks[2]) chunks[2].position = new Vector3(blx, bly + stepY, chunks[2].position.z); // TL
+        if (chunks[3]) chunks[3].position = new Vector3(blx + stepX, bly + stepY, chunks[3].position.z); // TR
+    }
+
+}
--- Assets/Scripts/UI/Reposition.cs
+++ Assets/Scripts/UI/Reposition.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    //¹ö±× ÀÌ½´·Î Æó±â
+    int tileSize = 48;
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (!collision.CompareTag("Area"))
@@ -13,7 +15,6 @@ public class Reposition : MonoBehaviour
         Vector3 myPos = transform.position;
 
 
-
         switch (transform.tag)
         {
             case "Ground":
@@ -23,13 +24,24 @@ public class Reposition : MonoBehaviour
                 float dirY = diffY < 0 ? -1 : 1;
                 diffX = Mathf.Abs(diffX);
                 diffY = Mathf.Abs(diffY);
+                //if(diffX >24f && diffY > 24f)
+                //{
+                //    //transform.Translate(Vector3.right * dirX * tileSize);
+                //    //transform.Translate(Vector3.up * dirY * tileSize);
+                //    transform.Translate(new Vector3(dirX * tileSize, dirY * tileSize, 0));
+                //    Debug.Log(name + " XX YY  " + "diffX: " + diffX + " diffY: " + diffY);
+
+                //}            
+                //else
                 if (diffX > diffY)
                 {
-                    transform.Translate(Vector3.right * dirX * 48);
+                    transform.Translate(Vector3.right * dirX * tileSize);
+                    Debug.Log(name + " XX  " + "diffX: " + diffX + " diffY: " + diffY);
                 }
                 else if(diffX < diffY)
                 {
-                    transform.Translate(Vector3.up * dirY * 48);
+                    transform.Translate(Vector3.up * dirY * tileSize);
+                    Debug.Log(name + " YY  " + "diffX: " + diffX + " diffY: " + diffY);
                 }
                 break;
             case "Enemy":
