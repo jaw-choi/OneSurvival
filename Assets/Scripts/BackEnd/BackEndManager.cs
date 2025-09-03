a7005e5fe19485f50d3c20ac295e243b1b913fee
jae <wod6983@naver.com>
2025-09-03 14:53:13 +0900

    Connect Server, Make Logo Scene
    logo scene 제작
    뒤끝 서버와 연결 완료

---
+++ Assets/Scripts/BackEnd/BackEndManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using BackEnd;
+
+public class BackEndManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        BackEndSetup();
+    }
+    private void BackEndSetup()
+    {
+        var bro = Backend.Initialize();
+
+        if(bro.IsSuccess())
+        {
+            Debug.Log($"초기화 성공 : {bro}");
+        }
+        else
+        {
+            Debug.LogError($"초기화 실패 : {bro}");
+
+        }
+    }
+}
---
+++ Assets/Scripts/BackEnd/LogoScenario.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoScenario : MonoBehaviour
+{
+    [SerializeField] private Progress progress;
+    private void Awake()
+    {
+        SystemSetup();
+    }
+    private void SystemSetup()
+    {
+        Application.runInBackground = true;
+
+        int width = Screen.width;
+        int height = (int)(Screen.width * 16f/9);
+        Screen.SetResolution(width, height, true);
+
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+        progress.Play(OnAfterProgress);
+    }
+    private void OnAfterProgress()
+    {
+        //SceneManager.LoadScene("MainMenu");
+    }
+}
